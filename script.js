1a70c0f4d50f3f12685d052df9ea2c104e6e95e6
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-07-17 12:31:19 +0200

    added function to icon button

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector("#icon");
+
+btn.addEventListener("click", scrollFunction);
+
+function scrollFunction(){
+
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+}
+
+
