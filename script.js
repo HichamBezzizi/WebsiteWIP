0855a7e1a44aae9ce79c516ea7155b69846d7c16
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-07-17 14:13:03 +0200

    add scroll to top function to icon

--- script.js
+++ script.js
@@ -1,12 +1,128 @@
-let btn = document.querySelector("#icon");
+const icon = document.querySelector("#icon");
 
-btn.addEventListener("click", scrollFunction);
-
+icon.addEventListener('click', scrollFunction);
 function scrollFunction(){
-
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+};
 
-}
+window.onload = function () {
+    window.scrollTo(0,0);
+};
+
+
+// // window.onscroll = function() {
+// //     onScrollDown();
+// //     onScrollUp();
+
+// // };
+
+// // function onScrollDown() {
+// //     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+// //       document.querySelector("#icon").style.display = "block";
+// //     } else {
+// //       document.querySelector("#icon").style.color = "white";
+// //     }
+// //   };
+
+// // function onScrollUp(){
+
+// //     if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0 ) {
+// //         document.querySelector("#icon").style.display = "block";
+// //     }
+
+// // };
+
+// // window.addEventListener('scroll', checkPosition);
+
+// // let scrollPos = 0;
+
+// // function checkPosition(){
+
+// //     let windowY = window.scrollY;
+
+// //     if( windowY < scrollPos ){
+
+// //         icon.style.display = "none";
+
+// //     } else{
+
+// //         icon.style.display = "block";
+
+// //     }
+
+// // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
