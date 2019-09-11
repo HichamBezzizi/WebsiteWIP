32e0ced671dde2c6ce720f7c54f2143e9864c4e1
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-09-11 09:39:38 +0200

    added func for icon to hide on scroll

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const icon = document.querySelector("#icon");
 
-icon.addEventListener('click', scrollFunction);
+icon.addEventListener("click", scrollFunction);
 
 function scrollFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-};
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 window.onload = function () {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 };
 
+
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 0) {
+    $('#icon').show();
+  }
+  else {
+    $('#icon').hide();
+  }
+});
 
 // window.onscroll = function() {
 //     onScrollDown();
