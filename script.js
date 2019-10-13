e6090fc1ceb7273e4caba71d30ae01ddc6857211
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-10-13 19:19:21 +0200

    .

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ window.onload = function () {
   window.scrollTo(0, 0);
 };
 
+
+
 $(window).scroll(function () {
   if ($(this).scrollTop() > 0) {
     $('#icon').show();
@@ -20,4 +22,44 @@ $(window).scroll(function () {
   }
 });
 
+// window.onscroll = function() {
+//     onScrollDown();
+//     onScrollUp();
 
+// };
+
+// function onScrollDown() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//       document.querySelector("#icon").style.display = "block";
+//     } else {
+//       document.querySelector("#icon").style.color = "white";
+//     }
+//   };
+
+// function onScrollUp(){
+
+//     if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0 ) {
+//         document.querySelector("#icon").style.display = "block";
+//     }
+
+// };
+
+// window.addEventListener('scroll', checkPosition);
+
+// let scrollPos = 0;
+
+// function checkPosition(){
+
+//     let windowY = window.scrollY;
+
+//     if( windowY < scrollPos ){
+
+//         icon.style.display = "none";
+
+//     } else{
+
+//         icon.style.display = "block";
+
+//     }
+
+// };
